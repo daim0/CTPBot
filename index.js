f401ad64b516d6ebf2914de152e3a3d8a91880f8
daim0 <daimplacespixels@gmail.com>
2020-07-01 12:03:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ client.on('message', message => {
         return message.channel.send("```" + 
         "CTP Bot:\n" +
         "Current version: 0.1.2.\n" + 
-        "Bot github: https://github.com/daim0/CTPBot" +
+        "Bot github: https://github.com/daim0/CTPBot\n" +
         "If you come across any errors notify daimgamer#6490 on discord." +
         "```");
     }
