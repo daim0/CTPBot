6b35fa3dc19b4f83809b6ea96a4c15f853f62d0d
FelipeVasquez350 <felipe.vasquez.lazzara@gmail.com>
2021-04-01 23:23:02 +0200

    Stop doing what you want

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ client.once('ready', () => {
     //jajaja old same old same
     setTimeout(unzip, 1000);
     ThroughDirectory("./CalamityTexturePack");
-    setTimeout(RunPythonScript, 7000);
+    setTimeout(RunPythonScript, 10000);
 });
 
 // Read messages.
